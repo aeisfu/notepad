0d65624a9005519f99d32ced5fd73666293128ce
aeisfu <78449268+aeisfu@users.noreply.github.com>
2021-02-12 17:45:58 -0500

    app.js

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 document.getElementById("heading").innerHTML =
   localStorage["title"] || "This is big text."; // default text
-document.getElementById("content").innerHTML =
-  localStorage["text"] || "This is small text."; // default text
 
 setInterval(function() {
   // fuction that is saving the innerHTML of the div
